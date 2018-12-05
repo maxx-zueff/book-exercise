b75d1bb24935f922d92839f37c45da619b880fac
Max Zueff <maxx.zueff@gmail.com>
2018-12-05 23:03:39 +0300

    completed exercise

--- 04-compound-types/task-0410.cpp
+++ 04-compound-types/task-0410.cpp
@@ -3,3 +3,28 @@ dash (or 40-meter, if you prefer) and then displays the times and the average.
 Use an array object to hold the data. (Use a built-in array if array is not
 available.) */
 
+#include <iostream>
+#include <array>
+
+int main() {
+
+	using namespace std;
+
+	array<float, 3> average;
+	float total;
+
+	for (int i = 0; i < 3; ++i) {
+		cout << "Enter 40-yd dash: ";
+		cin >> average[i];
+	}
+
+	cout << "Result:" << endl;
+	for (int i = 0; i < 3; ++i) {
+		cout << average[i] << " ";
+		total = total + average[i];
+	}
+
+	cout << "Average: " << total/3;
+
+	return 0;
+}
